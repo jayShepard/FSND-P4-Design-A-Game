cb8369cb9f7469d9a0e18cd3a56ee92d99de4323
Jayden Shepard <jaydenshepard@gmail.com>
2016-03-22 10:47:21 -0400

    get_game api point working

--- Minesweeper/api.py
+++ Minesweeper/api.py
@@ -66,7 +66,7 @@ class MineSweeperApi(remote.Service):
         # so it is performed out of sequence.
         #taskqueue.add(url='/tasks/cache_average_attempts')
         return game.to_form('Good luck playing Guess a Number!')
-'''
+
     @endpoints.method(request_message=GET_GAME_REQUEST,
                       response_message=GameForm,
                       path='game/{urlsafe_game_key}',
@@ -79,7 +79,7 @@ class MineSweeperApi(remote.Service):
             return game.to_form('Time to make a move!')
         else:
             raise endpoints.NotFoundException('Game not found!')
-
+'''
     @endpoints.method(request_message=MAKE_MOVE_REQUEST,
                       response_message=GameForm,
                       path='game/{urlsafe_game_key}',
